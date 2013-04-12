eccd16cd5c5064b4b03be16c3e81984035f9ae70
Andreas Johansson <andreas@stdin.se>
2013-04-12 10:50:46 +0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Digilys::Application.config.secret_token = '293f5e2289d1e5cf28216ee3ae495cabb9cef960e7b8c7e7d7046d49db75e38004d44720a9af0c8c953b17df24a76e6878c7e5158f6830f687273c5e0ae57dc7'
