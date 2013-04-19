223cddc46dea827b156727c1f48719040d7984ec
Andreas Johansson <andreas@stdin.se>
2013-04-19 08:39:16 +0200

    Name change, Digilys -> DigiLys

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -7,7 +7,7 @@ RailsAdmin.config do |config|
   ################  Global configuration  ################
 
   # Set the admin name here (optional second array element will appear in red). For example:
-  config.main_app_name = ['Digilys', 'Admin']
+  config.main_app_name = ['DigiLys', 'Admin']
   # or for a more dynamic name:
   # config.main_app_name = Proc.new { |controller| [Rails.application.engine_name.titleize, controller.params['action'].titleize] }
 
