953bf6342c0310d5ded78adf8cf94b6745ad6738
Andreas Johansson <andreas@stdin.se>
2013-04-23 13:55:57 +0200

    Catch-all 404 for ActiveRecord::RecordNotFound

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  private
+
+  # This picks up any layout set in the inheriting controller
+  def record_not_found
+    render :template => "shared/404", :status => 404
+  end
 end
