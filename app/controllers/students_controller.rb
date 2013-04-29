67a92efdef3c9713699c580e11168646733255f8
Andreas Johansson <andreas@stdin.se>
2013-04-29 13:48:43 +0200

    new/create for student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,4 +9,19 @@ class StudentsController < ApplicationController
     @students = Student.page(params[:page]).search(params[:q]).result
     render json: @students.collect { |s| { id: s.id, text: s.name } }.to_json
   end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(params[:student])
+
+    if @student.save
+      flash[:success] = t(:"students.create.success")
+      redirect_to @student
+    else
+      render action: "new"
+    end
+  end
 end
