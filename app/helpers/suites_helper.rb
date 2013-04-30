4942f2f0ae9df7d18958b60a52319c7c6e91db92
Andreas Johansson <andreas@stdin.se>
2013-04-30 13:25:59 +0200

    Added calendar listing for suites.

--- app/helpers/suites_helper.rb
+++ app/helpers/suites_helper.rb
@@ -1,2 +1,11 @@
 module SuitesHelper
+
+  # Generates a sorted list of the entries that
+  # should appear in the suite calendar
+  def calendar_entries(suite)
+    (
+      suite.evaluations +
+        suite.meetings
+    ).sort_by(&:date)
+  end
 end
--- spec/helpers/suites_helper_spec.rb
+++ spec/helpers/suites_helper_spec.rb
@@ -11,4 +11,35 @@ require 'spec_helper'
 #   end
 # end
 describe SuitesHelper do
+  context "#calendar_entries" do
+    let(:suite) { create(:suite) }
+    subject { helper.calendar_entries(suite) }
+
+    context "without entites" do
+      it { should be_empty }
+    end
+
+    context "with entities" do
+      let(:entities) do
+        [
+          create(:evaluation, suite: suite, date: Date.yesterday),
+          create(:evaluation, suite: suite, date: Date.today),
+          create(:evaluation, suite: suite, date: Date.tomorrow),
+          create(:meeting,    suite: suite, date: Date.yesterday),
+          create(:meeting,    suite: suite, date: Date.today),
+          create(:meeting,    suite: suite, date: Date.tomorrow)
+        ]
+      end
+
+      it { should match_array(entities) }
+
+      it "sorts the collection by the entities's date" do
+        prev = nil
+        subject.each do |e|
+          e.date.should >= prev.date unless prev.nil?
+          prev = e
+        end
+      end
+    end
+  end
 end
