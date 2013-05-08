2ae2d627346296abdca1eba37b21640bfe4dda2f
Andreas Johansson <andreas@stdin.se>
2013-05-08 08:58:15 +0200

    Specs for the report action

--- spec/controllers/evaluations_controller_spec.rb
+++ spec/controllers/evaluations_controller_spec.rb
@@ -1,4 +1,24 @@
 require 'spec_helper'
 
 describe EvaluationsController do
+  describe "GET #report" do
+    let!(:suite)        { create(:suite) }
+    let!(:evaluation)   { create(:evaluation, suite: suite) }
+    let!(:participants) { create_list(:participant, 2, suite: suite)}
+
+    it "generates unsaved results for participants" do
+      get :report, id: evaluation.id
+      assigns(:evaluation).results.collect(&:student).should match_array(participants.collect(&:student))
+      assigns(:evaluation).results.collect(&:new_record?).should == [ true, true ]
+    end
+    it "does not generate results if they already exist" do
+      results = [
+        create(:result, student: participants.first.student,  evaluation: evaluation),
+        create(:result, student: participants.second.student, evaluation: evaluation)
+      ]
+
+      get :report, id: evaluation.id
+      assigns(:evaluation).results.should match_array(results)
+    end
+  end
 end
