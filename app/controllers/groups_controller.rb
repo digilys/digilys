7613df77927cb75a50c01744b00a67832fe0d8a9
Andreas Johansson <andreas@stdin.se>
2013-05-17 08:43:39 +0200

    Authorization base for groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,16 +1,17 @@
 class GroupsController < ApplicationController
   layout "admin"
 
+  load_and_authorize_resource
+
   def index
-    @groups = Group.top_level.order(:name).all
+    @groups = @groups.top_level.order(:name).all
   end
 
   def show
-    @group = Group.find(params[:id])
   end
 
   def search
-    @groups = Group.with_parents(2).page(params[:page]).search(params[:q]).result
+    @groups = @groups.with_parents(2).page(params[:page]).search(params[:q]).result
 
     render json: @groups.collect { |s|
       text = [ s.name, s.parent.try(:name), s.parent.try(:parent).try(:name) ].compact.join(", ")
@@ -19,12 +20,9 @@ class GroupsController < ApplicationController
   end
 
   def new
-    @group = Group.new
   end
 
   def create
-    @group = Group.new(params[:group])
-
     if @group.save
       flash[:success] = t(:"groups.create.success")
       redirect_to @group
@@ -34,12 +32,9 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
   end
 
   def update
-    @group = Group.find(params[:id])
-
     if @group.update_attributes(params[:group])
       flash[:success] = t(:"groups.update.success")
       redirect_to @group
@@ -49,33 +44,28 @@ class GroupsController < ApplicationController
   end
 
   def confirm_destroy
-    @group = Group.find(params[:id])
   end
   def destroy
-    group = Group.find(params[:id])
-    group.destroy
+    @group.destroy
 
     flash[:success] = t(:"groups.destroy.success")
     redirect_to groups_url()
   end
 
   def select_students
-    @group = Group.find(params[:id])
   end
 
   def add_students
-    group = Group.find(params[:id])
-    group.add_students(params[:group][:students])
+    @group.add_students(params[:group][:students])
 
     flash[:success] = t(:"groups.add_students.success")
-    redirect_to group
+    redirect_to @group
   end
 
   def remove_students
-    group = Group.find(params[:id])
-    group.remove_students(params[:student_ids])
+    @group.remove_students(params[:student_ids])
 
     flash[:success] = t(:"groups.remove_students.success")
-    redirect_to group
+    redirect_to @group
   end
 end
