b90396ab6a5e6a60d7d58fe3fa6ab72a0b775b34
Andreas Johansson <andreas@stdin.se>
2013-05-17 09:10:35 +0200

    Restructured the specs for ApplicationController.
    Don't use an anonymous controller where it's not needed.

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -3,13 +3,6 @@ require 'spec_helper'
 # https://www.relishapp.com/rspec/rspec-rails/v/2-4/docs/controller-specs/anonymous-controller
 describe ApplicationController do
   describe "#process_participant_autocomplete_params" do
-    controller do
-      def index
-        @result = process_participant_autocomplete_params(params[:testdata])
-        render nothing: true
-      end
-    end
-
     let(:students) { create_list(:student, 3) }
     let(:groups)   { create_list(:group, 2) }
 
@@ -18,53 +11,58 @@ describe ApplicationController do
       groups.second.students << students.second
     end
 
-    it "returns nil when there are no params" do
-      get :index, testdata: nil
-      assigns(:result).should be_nil
+    let(:params)     { nil }
+    subject(:result) { controller.send(:process_participant_autocomplete_params, params) }
+
+    it { should be_nil }
+
+    context "with comma separated student ids" do
+      let(:params) { { student_id: "2,3" } }
+      it { should match_array([ { student_id: 2 }, { student_id: 3 } ]) }
+    end
+    context "with a single student id" do
+      let(:params) { { student_id: "2" } }
+      it { should match_array([ { student_id: 2 } ]) }
     end
 
-    it "converts comma separated student ids" do
-      get :index, testdata: { student_id: "2,3" }
-      assigns(:result).should match_array([ { "student_id" => 2 }, { "student_id" => 3 } ])
+    context "with comma separated group ids" do
+      let(:params) { { group_id: "#{groups.first.id},#{groups.second.id}" } }
+      it {
+        should match_array([
+          { group_id: groups.first.id,  student_id: students.first.id },
+          { group_id: groups.second.id, student_id: students.second.id }
+        ])
+      }
     end
-    it "handles a single student id" do
-      get :index, testdata: { student_id: "2" }
-      assigns(:result).should match_array([ { "student_id" => 2 } ])
-    end
-
-    it "converts comma separated group ids" do
-      get :index, testdata: { group_id: "#{groups.first.id},#{groups.second.id}" }
-      assigns(:result).should match_array([
-        { "group_id" => groups.first.id,  "student_id" => students.first.id },
-        { "group_id" => groups.second.id, "student_id" => students.second.id }
-      ])
-    end
-    it "handles a single group id" do
-      get :index, testdata: { group_id: "#{groups.first.id}" }
-      assigns(:result).should match_array([
-        { "group_id" => groups.first.id, "student_id" => students.first.id }
-      ])
+    context "with a single group id" do
+      let(:params) { { group_id: "#{groups.first.id}" } }
+      it {
+        should match_array([
+          { group_id: groups.first.id, student_id: students.first.id }
+        ])
+      }
     end
 
-    it "overrides an explicit student id if that student is also included from the group ids" do
-      get :index, testdata: { group_id: "#{groups.first.id}", student_id: "#{students.first.id}" }
-      assigns(:result).should match_array([
-        { "group_id" => groups.first.id,  "student_id" => students.first.id }
-      ])
+    context "with the same student id explicit and from groups" do
+      let(:params) { { group_id: "#{groups.first.id}", student_id: "#{students.first.id}" } }
+      it {
+        should match_array([
+          { group_id: groups.first.id,  student_id: students.first.id }
+        ])
+      }
     end
 
-    it "ignores duplicates" do
-      get :index, testdata: { group_id: "#{groups.first.id},#{groups.first.id}", student_id: "#{students.first.id},#{students.first.id}" }
-      assigns(:result).should match_array([
-        { "group_id" => groups.first.id, "student_id" => students.first.id }
-      ])
+    context "with duplicates" do
+      let(:params) { { group_id: "#{groups.first.id},#{groups.first.id}", student_id: "#{students.first.id},#{students.first.id}" } }
+      it {
+        should match_array([
+          { group_id: groups.first.id, student_id: students.first.id }
+        ])
+      }
     end
   end
 
   describe "#results_to_datatable" do
-    controller do
-    end
-
     let!(:students)     { create_list(:student, 2) }
     let!(:evaluations)  { create_list(:evaluation, 2, max_result: 10, red_below: 4, green_above: 7) }
     let!(:result_s1_e1) { create(:result, evaluation: evaluations.first,  student: students.first,  value: 4) }
