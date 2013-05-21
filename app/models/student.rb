a25765ce78a009a4fbafce251d28536fd64c8925
Andreas Johansson <andreas@stdin.se>
2013-05-21 12:16:26 +0200

    Added a simple text format for working with a student's data field.

--- app/models/student.rb
+++ app/models/student.rb
@@ -12,7 +12,8 @@ class Student < ActiveRecord::Base
     :first_name,
     :last_name,
     :gender,
-    :data
+    :data,
+    :data_text
 
   validates :personal_id, presence: true, uniqueness: true
   validates :first_name,  presence: true
@@ -20,11 +21,26 @@ class Student < ActiveRecord::Base
 
   enumerize :gender, in: [ :male, :female ]
 
+
+  serialize   :data, JSON
+  validate    :validate_data_text
+  before_save :convert_data_text_to_data
+
   def name
     "#{self.first_name} #{self.last_name}"
   end
   def name_was
     "#{self.first_name_was} #{self.last_name_was}"
+  end
+
+  def data_text
+    if !@data_text && !self.data.blank?
+      @data_text = self.data.collect { |key, value| "#{key}: #{json_value_to_text_value(value)}" }.join("\n")
+    end
+    return @data_text
+  end
+  def data_text=(value)
+    @data_text = value
   end
 
   # Add this user to all of the groups, and all of the group's parents
@@ -46,5 +62,74 @@ class Student < ActiveRecord::Base
       group = g.is_a?(Group) ? g : Group.find(g)
       group.remove_students(self)
     end
+  end
+
+
+  private
+
+  def validate_data_text
+    data_text = self.data_text
+    return if data_text.blank?
+
+    line_num = 1
+    faulty_lines = []
+
+    data_text.each_line do |line|
+      faulty_lines << line_num if !line.blank? && line.count(":") != 1
+      line_num += 1
+    end
+
+    errors.add(:data_text, :faulty_lines, lines: faulty_lines.join(",")) unless faulty_lines.blank?
+  end
+
+  def convert_data_text_to_data
+    data_text = self.data_text
+    return if data_text.blank?
+
+    data = {}
+
+    data_text.each_line do |line|
+      next if line.blank?
+
+      key, value = line.split(":").collect(&:strip)
+      data[key] = text_value_to_json_value(value)
+    end
+
+    self.data = data
+  end
+
+  def json_value_to_text_value(value)
+    case value
+    when Numeric
+      value.to_s.gsub(".", ",")
+    when true
+      "Ja"
+    when false
+      "Nej"
+    else
+      value.to_s
+    end
+  end
+
+  def text_value_to_json_value(value)
+    maybe_numeric = value.gsub(",", ".").gsub(/\s/, "")
+
+    case maybe_numeric
+    when /^[+-]?\d+.\d*$/ # Float
+      return maybe_numeric.to_f
+    when /^[+-]?\d+$/ # Int
+      return maybe_numeric.to_i
+    end
+
+    stripped = value.strip
+
+    case stripped
+    when /^(ja|yes|sant|true)$/i
+      return true
+    when /^(nej|no|falskt|false)$/i
+      return false
+    end
+
+    return value
   end
 end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -19,10 +19,140 @@ describe Student do
     it { should validate_presence_of(:last_name) }
     it { should ensure_inclusion_of(:gender).in_array(%w(male female)) }
   end
+  context ".validate_data_text" do
+    it { should allow_value(nil).for(:data_text) }
+    it { should allow_value("foo: bar\nbar: baz").for(:data_text) }
+    it { should allow_value("\nfoo: bar\n\n   \n\nbar: baz\n").for(:data_text) }
+
+    it { should_not allow_value("foo\nbar: baz").for(:data_text).with_message(/rad 1/i) }
+    it { should_not allow_value("foo: bar\nbar: baz: foo").for(:data_text).with_message(/rad 2/i) }
+    it { should_not allow_value("foo\nbar: baz: foo").for(:data_text).with_message(/rad 1,2/i) }
+  end
 
   context ".name" do
     subject { create(:student, first_name: "Foo", last_name: "Bar") }
     its(:name) { should == "Foo Bar" }
+  end
+
+  context ".data_text=" do
+    subject { create(:student, data_text: text) }
+
+    context "with multiple rows" do
+      let(:text) { "foo: bar\nbar: baz" }
+      its(:data) { should have(2).items }
+      its(:data) { should include("foo" => "bar") }
+      its(:data) { should include("bar" => "baz") }
+    end
+    context "with empty rows" do
+      let(:text) { "\n\nfoo: bar\n\n   \n\nbar: baz\n\n" }
+      its(:data) { should have(2).items }
+    end
+    context "with an integer" do
+      let(:text) { "foo: 1" }
+      its(:data) { should == { "foo" => 1 } }
+    end
+    context "with a float" do
+      context "using dots" do
+        let(:text) { "foo: 1.2" }
+        its(:data) { should == { "foo" => 1.2 } }
+      end
+      context "using comma" do
+        let(:text) { "foo: 1,3" }
+        its(:data) { should == { "foo" => 1.3 } }
+      end
+    end
+    context "with a true value" do
+      context "using ja" do
+        let(:text) { "foo: ja" }
+        its(:data) { should == { "foo" => true } }
+      end
+      context "using Ja" do
+        let(:text) { "foo: Ja" }
+        its(:data) { should == { "foo" => true } }
+      end
+      context "using yes" do
+        let(:text) { "foo: yes" }
+        its(:data) { should == { "foo" => true } }
+      end
+      context "using Yes" do
+        let(:text) { "foo: Yes" }
+        its(:data) { should == { "foo" => true } }
+      end
+      context "using sant" do
+        let(:text) { "foo: sant" }
+        its(:data) { should == { "foo" => true } }
+      end
+      context "using Sant" do
+        let(:text) { "foo: Sant" }
+        its(:data) { should == { "foo" => true } }
+      end
+      context "using true" do
+        let(:text) { "foo: true" }
+        its(:data) { should == { "foo" => true } }
+      end
+      context "using True" do
+        let(:text) { "foo: True" }
+        its(:data) { should == { "foo" => true } }
+      end
+    end
+    context "with a false value" do
+      context "using nej" do
+        let(:text) { "foo: nej" }
+        its(:data) { should == { "foo" => false } }
+      end
+      context "using Nej" do
+        let(:text) { "foo: Nej" }
+        its(:data) { should == { "foo" => false } }
+      end
+      context "using no" do
+        let(:text) { "foo: no" }
+        its(:data) { should == { "foo" => false } }
+      end
+      context "using No" do
+        let(:text) { "foo: No" }
+        its(:data) { should == { "foo" => false } }
+      end
+      context "using falskt" do
+        let(:text) { "foo: falskt" }
+        its(:data) { should == { "foo" => false } }
+      end
+      context "using Falskt" do
+        let(:text) { "foo: Falskt" }
+        its(:data) { should == { "foo" => false } }
+      end
+      context "using false" do
+        let(:text) { "foo: false" }
+        its(:data) { should == { "foo" => false } }
+      end
+      context "using False" do
+        let(:text) { "foo: False" }
+        its(:data) { should == { "foo" => false } }
+      end
+    end
+  end
+  context ".data_text" do
+    subject { create(:student, data: data) }
+
+    context "with nil" do
+      let(:data) { nil }
+      its(:data_text) { should be_nil }
+    end
+    context "with multiple values" do
+      let(:data) { { foo: "bar", bar: 1 } }
+      its(:data_text) { should == "foo: bar\nbar: 1" }
+    end
+    context "with float" do
+      let(:data) { { foo: 1.2 } }
+      its(:data_text) { should == "foo: 1,2" }
+    end
+    context "with true" do
+      let(:data) { { foo: true } }
+      its(:data_text) { should == "foo: Ja" }
+    end
+    context "with false" do
+      let(:data) { { foo: false } }
+      its(:data_text) { should == "foo: Nej" }
+    end
   end
 
   context ".add_to_groups" do
