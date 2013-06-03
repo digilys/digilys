d4f84a5e5f213db8bdca11631a2dc9432fdab0d7
Andreas Johansson <andreas@stdin.se>
2013-06-03 09:45:44 +0200

    Added suite list to the landing page

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,7 +1,18 @@
 class IndexController < ApplicationController
-  skip_authorization_check only: :index
-  authorize_resource class: false, except: :index
+  skip_authorization_check
 
   def index
+    load_dashboard_data if user_signed_in?
+  end
+
+
+  private
+
+  def load_dashboard_data
+    @suites = Suite.regular.order(:updated_at).limit(10)
+
+    unless current_user.has_role?(:admin)
+      @suites.with_role(:suite_manager, current_user)
+    end
   end
 end
