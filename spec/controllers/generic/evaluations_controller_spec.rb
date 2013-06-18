9e0b548c6665f40229024376e0584864cb5822ec
Andreas Johansson <andreas@stdin.se>
2013-06-18 10:21:20 +0200

    Specs for {Generic,Template}::EvaluationsController

--- spec/controllers/generic/evaluations_controller_spec.rb
+++ spec/controllers/generic/evaluations_controller_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe Generic::EvaluationsController do
+  login_admin
 
+  describe "GET #index" do
+    let!(:generics) { create_list(:generic_evaluation,  2) }
+    let!(:others)   { create_list(:evaluation_template, 2) }
+
+    it "gives a list of generic evaluations" do
+      get :index
+      response.should be_success
+      assigns(:evaluations).should match_array(generics)
+    end
+  end
+
+  describe "GET #new" do
+    it "builds a generic evaluation" do
+      get :new
+      response.should be_success
+      assigns(:evaluation).type.to_sym.should == :generic
+    end
+  end
 end
--- spec/controllers/template/evaluations_controller_spec.rb
+++ spec/controllers/template/evaluations_controller_spec.rb
@@ -1,5 +1,43 @@
 require 'spec_helper'
 
 describe Template::EvaluationsController do
+  login_admin
 
+  describe "GET #index" do
+    let!(:templates) { create_list(:evaluation_template, 2) }
+    let!(:others)    { create_list(:generic_evaluation,  2) }
+
+    it "gives a list of generic evaluations" do
+      get :index
+      response.should be_success
+      assigns(:evaluations).should match_array(templates)
+    end
+  end
+
+  describe "GET #search" do
+    let(:template) { create(:evaluation_template) }
+    let!(:other)   { [ create(:evaluation_template), create(:generic_evaluation, name: template.name) ] }
+
+    it "returns matching template evaluations as json" do
+      get :search, q: { name_cont: template.name }
+
+      response.should be_success
+      json = JSON.parse(response.body)
+
+      json["more"].should be_false
+
+      json["results"].should have(1).items
+      json["results"].first.should include("id" =>          template.id)
+      json["results"].first.should include("name" =>        template.name)
+      json["results"].first.should include("description" => template.description)
+    end
+  end
+
+  describe "GET #new" do
+    it "builds a template evaluation" do
+      get :new
+      response.should be_success
+      assigns(:evaluation).type.to_sym.should == :template
+    end
+  end
 end
