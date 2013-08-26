05419a9bcaa31ef1d45afeda100517a30791f770
Andreas Johansson <andreas@stdin.se>
2013-08-26 11:35:12 +0200

    Changed Capistrano to enable deploy to different environments

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,17 @@
 require "bundler/capistrano"
 
+# Digilys is deployed to a single server. The name of the server
+# can either be supplied as an environment variable +digilys_instance+
+# when executing cap:
+#
+#   > digilys_instance=digilys-production-alternative cap deploy
+#
+# or it defaults to the server +digilys_instance+.
+#
+# A good idea is to have an +~/.ssh/config+ which includes server details
+# fo
+set :digilys_server,   ENV["digilys_instance"] || "digilys-production"
+
 set :application,      "digilys"
 set :scm,              :git
 set :use_sudo,         false
@@ -9,18 +21,12 @@ set :repository,       "."
 set :local_repository, "."
 set :deploy_via,       :copy
 
-set :deploy_to,        "/home/digilys/app"
+set :deploy_to,        -> { capture("echo -n $HOME/app") }
 
 
-# As of now, deploying requires you to have the following virtual hosts
-# setup in your ~/.ssh/config:
-#
-# - digilys-production
-#
-# The server"s different roles can be seen below
-role :web, "digilys-production"                # Your HTTP server, Apache/etc
-role :app, "digilys-production"                # This may be the same as your `Web` server
-role :db,  "digilys-production", primary: true # This is where Rails migrations will run
+role :web, digilys_server                # Your HTTP server, Apache/etc
+role :app, digilys_server                # This may be the same as your `Web` server
+role :db,  digilys_server, primary: true # This is where Rails migrations will run
 
 # if you want to clean up old releases on each deploy uncomment this:
 after "deploy:restart", "deploy:cleanup"
