f6c519ce705fff17d672830089437a7e58a1f9a0
Andreas Johansson <andreas@stdin.se>
2013-09-02 11:56:24 +0200

    Added Capistrano task for running rake tasks.

--- config/deploy.rb
+++ config/deploy.rb
@@ -65,4 +65,9 @@ namespace :deploy do
       run "ln -nsf #{latest_release}/public #{latest_release}/public#{root_url}"
     end
   end
+
+  desc "Invoke rake task"
+  task :invoke do
+    run "cd '#{current_path}' && #{rake} #{ENV['task']} RAILS_ENV=#{rails_env}"
+  end
 end
