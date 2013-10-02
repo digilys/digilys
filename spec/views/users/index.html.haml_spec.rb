012a0ba10fc25e3e9a19c12467973ac88b2de9d6
Andreas Johansson <andreas@stdin.se>
2013-10-02 08:52:38 +0200

    Fix broken specs

--- spec/views/users/index.html.haml_spec.rb
+++ spec/views/users/index.html.haml_spec.rb
@@ -4,7 +4,7 @@ describe "users/index" do
   let(:users) { create_list(:user, 2) }
   before(:each) do
     view.stub(:current_user).and_return(users.first)
-    assign(:users, users)
+    assign(:users, Kaminari.paginate_array(users).page(1).per(10))
     render
   end
 
