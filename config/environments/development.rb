25dc71836af8e1f3204b74498d9b8046fdc7f333
Andreas Johansson <andreas@stdin.se>
2014-01-28 09:36:47 +0100

    Increase speed in development mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,7 @@ Digilys::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = true
+  config.assets.debug = false
 
   config.cache_store = :dalli_store
 end
