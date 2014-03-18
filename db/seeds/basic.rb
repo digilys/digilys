49fb90b4b2472765362deac63de1e9c0bf29dec0
Andreas Johansson <andreas@stdin.se>
2014-03-18 10:48:56 +0100

    Added two development seeds

---
+++ db/seeds/basic.rb
@@ -0,0 +1,134 @@
+## Development base scenario
+#
+# Run app:factory:students first
+#
+# 3 users:
+# - 1 superuser
+# - 1 reader (regular user)
+# - 1 editor (regular user)
+# 
+# 1 evaluation template
+#
+# 2 generic evaluations, booleans
+#
+# 1 group, adds the first 5 students to the group
+#
+# Data added to the first student:
+# - Student data, a few different keys and values
+# - Results for the two generic evaluations
+
+instance = Instance.order("id asc").first
+
+puts "Superuser"
+user = User.new do |u|
+  u.name                  = "Superuser"
+  u.email                 = "superuser@example.com"
+  u.password              = "testtest"
+  u.password_confirmation = "testtest"
+  u.active_instance       = instance
+end
+
+user.save!
+user.add_role :superuser
+
+puts "Reader"
+user = User.new do |u|
+  u.name                  = "Reader"
+  u.email                 = "reader@example.com"
+  u.password              = "testtest"
+  u.password_confirmation = "testtest"
+  u.active_instance       = instance
+end
+
+user.save!
+
+puts "Editor"
+user = User.new do |u|
+  u.name                  = "Editor"
+  u.email                 = "editor@example.com"
+  u.password              = "testtest"
+  u.password_confirmation = "testtest"
+  u.active_instance       = instance
+end
+
+user.save!
+
+puts "Evaluation template"
+Evaluation.create!({
+  instance:      instance,
+  type:          "template",
+  name:          "Test template",
+  description:   "",
+  target:        "all",
+  category_list: "",
+  max_result:    "8",
+  yellow_min:    "3",
+  yellow_max:    "5",
+  red_min:       "0",
+  red_max:       "2",
+  green_min:     "6",
+  green_max:     "8",
+  stanine1_min:  "0",
+  stanine1_max:  "0",
+  stanine2_min:  "1",
+  stanine2_max:  "1",
+  stanine3_min:  "2",
+  stanine3_max:  "2",
+  stanine4_min:  "3",
+  stanine4_max:  "3",
+  stanine5_min:  "4",
+  stanine5_max:  "4",
+  stanine6_min:  "5",
+  stanine6_max:  "5",
+  stanine7_min:  "6",
+  stanine7_max:  "6",
+  stanine8_min:  "7",
+  stanine8_max:  "7",
+  stanine9_min:  "8",
+  stanine9_max:  "8"
+})
+
+puts "Generic evaluations"
+generic1 = Evaluation.create!({
+  instance:        instance,
+  type:            "generic",
+  name:            "Boolean 1",
+  description:     "",
+  value_type:      "boolean",
+  color_for_true:  "green",
+  color_for_false: "red"
+})
+
+generic2 = Evaluation.create!({
+  instance:        instance,
+  type:            "generic",
+  name:            "Boolean 2",
+  description:     "",
+  value_type:      "boolean",
+  color_for_true:  "red",
+  color_for_false: "green"
+})
+
+puts "Group 1"
+group = Group.create!({
+  instance: instance,
+  name:     "Group 1"
+})
+group.add_students(Student.order("first_name asc, last_name asc").limit(5))
+
+puts "Student data"
+student = Student.order("first_name asc, last_name asc").first
+student.data_text = "Foo: Bar\nBar: Baz\nApa: Bepa\nBepa: Cepa\nCepa: Depa\nDepa: Epa\nEpa: Fepa"
+student.save!
+
+puts "Generic evaluation results"
+Result.create!(
+  student_id:    student.id,
+  evaluation_id: generic1.id,
+  value:         "0"
+)
+Result.create!(
+  student_id:    student.id,
+  evaluation_id: generic2.id,
+  value:         "0"
+)
---
+++ db/seeds/massive_color_table.rb
@@ -0,0 +1,90 @@
+## Creates a massive color table
+#
+# Run app:factory:students before
+#
+# 1 suite
+# 40 evaluations belonging to the suite
+# All students become participants in the suite
+# Random result for all students in all evaluations
+
+instance = Instance.order("id asc").first
+
+puts "Suite"
+suite = Suite.new do |s|
+  s.name     = "Massive color table"
+  s.instance = instance
+end
+
+puts "Evaluations"
+evaluations = ((Date.today - 39.days)..(Date.today)).collect do |date|
+  print "."
+  Evaluation.create!({
+    suite:         suite,
+    date:          date,
+    type:          "suite",
+    name:          "Test #{date}",
+    description:   "",
+    target:        "all",
+    category_list: "",
+    max_result:    "8",
+    yellow_min:    "3",
+    yellow_max:    "5",
+    red_min:       "0",
+    red_max:       "2",
+    green_min:     "6",
+    green_max:     "8",
+    stanine1_min:  "0",
+    stanine1_max:  "0",
+    stanine2_min:  "1",
+    stanine2_max:  "1",
+    stanine3_min:  "2",
+    stanine3_max:  "2",
+    stanine4_min:  "3",
+    stanine4_max:  "3",
+    stanine5_min:  "4",
+    stanine5_max:  "4",
+    stanine6_min:  "5",
+    stanine6_max:  "5",
+    stanine7_min:  "6",
+    stanine7_max:  "6",
+    stanine8_min:  "7",
+    stanine8_max:  "7",
+    stanine9_min:  "8",
+    stanine9_max:  "8"
+  })
+end
+puts ""
+
+puts "Participants"
+Student.all.each do |student|
+  print "."
+  Participant.create!({
+    student_id: student.id,
+    suite_id:   suite.id,
+  })
+end
+puts ""
+
+puts "Results"
+evaluations.each do |evaluation|
+  Student.all.each do |student|
+    print "."
+    value = rand(9) - 1
+
+    if value >= 0
+      Result.create!({
+        student_id:    student.id,
+        evaluation_id: evaluation.id,
+        value:         value
+      })
+    else
+      Result.create!({
+        student_id:    student.id,
+        evaluation_id: evaluation.id,
+        value:         nil,
+        absent:        true
+      })
+    end
+  end
+end
+puts ""
