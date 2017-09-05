128d2ed66cf89022a521c47a33482126b730b700
Per Garden <per.garden@avalonenterprise.com>
2017-09-05 13:13:50 +0200

    Not trusting Conphusion passenger for asset pipilining

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,10 @@ Digilys::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  # config.serve_static_assets = false
+  # Conphusion passenger sucks - see /var/log/nginx/error.log
+  # WEBRick solves the problem :-)
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
